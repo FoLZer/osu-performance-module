222295e56e28d60bef88bcc94423b92c769edd13
FoLZer <nikolaenko.gleb.044@gmail.com>
2021-09-27 14:22:37 +0300

    Fix npm install for windows

--- inst.js
+++ inst.js
@@ -3,4 +3,4 @@ const fs = require("fs");
 
 const p = path.resolve(".","src","osu-performance","dependencies","tinylogger","tinylogger","tinylogger.h");
 const c = fs.readFileSync(p, { encoding: 'utf-8' });
-fs.writeFileSync(p.replace(/ std::max/g, ' (std::max)'), c, { encoding: 'utf-8' });
+fs.writeFileSync(p, c.replace(/ std::max/g, ' (std::max)'), { encoding: 'utf-8' });
